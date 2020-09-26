366bad92cb5d4795184b51f814978231a68f7f62
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-26 21:38:03 +0000

    Deploy UCI-Ice-Hockey/UCI-Ice-Hockey.github.io to github.com/UCI-Ice-Hockey/UCI-Ice-Hockey.github.io.git:master

---
+++ precache-manifest.089ec3b89be79560390464ecfd1bee45.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1363d0084134143547d1db7e4cdc3d94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0be2b790852ec621d209",
+    "url": "/static/css/main.491f3ada.chunk.css"
+  },
+  {
+    "revision": "088abefa5bc4cdf631ae",
+    "url": "/static/js/2.22eb6125.chunk.js"
+  },
+  {
+    "revision": "618d9e067b5f7c0ee048a43ae1109b7d",
+    "url": "/static/js/2.22eb6125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0be2b790852ec621d209",
+    "url": "/static/js/main.d6381860.chunk.js"
+  },
+  {
+    "revision": "dc9c08830479cbb4fc7d",
+    "url": "/static/js/runtime-main.3472fe02.js"
+  },
+  {
+    "revision": "1052d6ca3993ae24a932304560a4c8b4",
+    "url": "/static/media/Abel-Regular.1052d6ca.ttf"
+  },
+  {
+    "revision": "a30c635b645d2137013cb574d84c77f7",
+    "url": "/static/media/acha_logo.a30c635b.png"
+  },
+  {
+    "revision": "3b197d54977a8825c78388db9a14d029",
+    "url": "/static/media/chapman-logo.3b197d54.png"
+  },
+  {
+    "revision": "61a77ea1a9f8a8c70361c21a68fc7b65",
+    "url": "/static/media/chapman-logo@128.61a77ea1.png"
+  },
+  {
+    "revision": "3b197d54977a8825c78388db9a14d029",
+    "url": "/static/media/chapman-logo@256.3b197d54.png"
+  },
+  {
+    "revision": "6a8c064ac45dfbe147d866263218de65",
+    "url": "/static/media/csub-logo.6a8c064a.png"
+  },
+  {
+    "revision": "308e4c00403decac7128f9873be658e0",
+    "url": "/static/media/csub-logo@128.308e4c00.png"
+  },
+  {
+    "revision": "eeb99c37417f2831eca9fb1680017d34",
+    "url": "/static/media/csub-logo@256.eeb99c37.png"
+  },
+  {
+    "revision": "1ef27b39e03dc5e395afb485f67e0959",
+    "url": "/static/media/csuf-logo.1ef27b39.png"
+  },
+  {
+    "revision": "05fb94627f32418e2cd3063b017e878e",
+    "url": "/static/media/csuf-logo@128.05fb9462.png"
+  },
+  {
+    "revision": "7900340796aa3c9cdcf6ed98d44827a3",
+    "url": "/static/media/csuf-logo@256.79003407.png"
+  },
+  {
+    "revision": "cd67fc97ce35dab4b272ff784d0a9410",
+    "url": "/static/media/goal.cd67fc97.jpg"
+  },
+  {
+    "revision": "4b8e28c20ef89ff4698ac7e3000c9cf5",
+    "url": "/static/media/great_park.4b8e28c2.jpg"
+  },
+  {
+    "revision": "4d46dc78beb4136ccfd7112cde60015e",
+    "url": "/static/media/ice-texture-1920.4d46dc78.jpg"
+  },
+  {
+    "revision": "cdcddb87f475fb55c596b63b327e4470",
+    "url": "/static/media/ice-texture-3840.cdcddb87.jpg"
+  },
+  {
+    "revision": "9fb8631527324f70a16c8a2e950c2643",
+    "url": "/static/media/practice.9fb86315.jpg"
+  },
+  {
+    "revision": "f21cbec8ef78372cd484cddd020a039f",
+    "url": "/static/media/team.f21cbec8.jpg"
+  },
+  {
+    "revision": "3521c89ac8b684bce95554905af0dd83",
+    "url": "/static/media/uci-logo_0.3521c89a.svg"
+  },
+  {
+    "revision": "0f594292f6e13bb96c0a1670f8d8c2bf",
+    "url": "/static/media/uci-logo_1.0f594292.svg"
+  },
+  {
+    "revision": "b4da62f065d20fdc048f0985dd5f1300",
+    "url": "/static/media/uci-logo_1.b4da62f0.png"
+  },
+  {
+    "revision": "d90f7341ea12b812775804681a463f7c",
+    "url": "/static/media/uci-logo_1@128.d90f7341.png"
+  },
+  {
+    "revision": "d8c86bc71fc0d41389a169bd1e93435c",
+    "url": "/static/media/uci-logo_1@256.d8c86bc7.png"
+  },
+  {
+    "revision": "db8c3d3341d70afd6b982beefd3a0f62",
+    "url": "/static/media/uci-logo_2.db8c3d33.svg"
+  },
+  {
+    "revision": "735b4f9d73c5e0a752b632dc58f5fe3a",
+    "url": "/static/media/uci-logo_3.735b4f9d.svg"
+  },
+  {
+    "revision": "7212bc3878d39bad79b32b246133eb39",
+    "url": "/static/media/ucla-logo.7212bc38.png"
+  },
+  {
+    "revision": "a5f79dba43cd3b962d58ad5a5712cbd2",
+    "url": "/static/media/ucla-logo@128.a5f79dba.png"
+  },
+  {
+    "revision": "6c7e41f2921090331dbcdb16248ed7cd",
+    "url": "/static/media/ucla-logo@256.6c7e41f2.png"
+  },
+  {
+    "revision": "ab5d3554cdc474d773a804a0ad963c54",
+    "url": "/static/media/ucsb-logo.ab5d3554.png"
+  },
+  {
+    "revision": "9e5290c614dd5faca0253bd83bc3aefa",
+    "url": "/static/media/ucsb-logo@128.9e5290c6.png"
+  },
+  {
+    "revision": "138eb53f92ff5084575f089198c024ab",
+    "url": "/static/media/ucsb-logo@256.138eb53f.png"
+  },
+  {
+    "revision": "b619b9512233854a80fd5041cad58aa0",
+    "url": "/static/media/ucsd-logo.b619b951.png"
+  },
+  {
+    "revision": "df57f10f5de8482024dcb767c7d53f18",
+    "url": "/static/media/ucsd-logo@128.df57f10f.png"
+  },
+  {
+    "revision": "769eba66e18f3b0d9fecf8a044ec36da",
+    "url": "/static/media/ucsd-logo@256.769eba66.png"
+  },
+  {
+    "revision": "34a39908ce26a9310215699c5e625398",
+    "url": "/static/media/usa_hockey_logo.34a39908.png"
+  },
+  {
+    "revision": "77974db8874989e4a55332a6a2ad747e",
+    "url": "/static/media/usd-logo.77974db8.png"
+  },
+  {
+    "revision": "eb0076de8dcf3c76d6962aaf667dc1a7",
+    "url": "/static/media/usd-logo@128.eb0076de.png"
+  },
+  {
+    "revision": "7ad887c291cad8ee5d0ad12a9ee5c8ce",
+    "url": "/static/media/usd-logo@256.7ad887c2.png"
+  },
+  {
+    "revision": "7718757c1fb44d42a44274543f69cb58",
+    "url": "/static/media/victory.7718757c.jpg"
+  }
+]);
